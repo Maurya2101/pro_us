da325cc2b093389a06b19271b42ffd79f198947c
MAURYA PATEL <mananp2101@gmail.com>
2024-03-19 11:58:09 +0530

    customerdashboard

--- user/urls.py
+++ user/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('book-now/<int:item_id>/', views.book_now, name='book_now'),
     path('invoice/<int:invoice_id>/', views.show_invoice, name='show_invoice'),
 
+    
+    
     # Other paths...
     path('submit/',submit, name='submit'),
     path('terms/',terms, name='terms'),
@@ -35,30 +37,3 @@ urlpatterns = [
 ]
 
 
-
-# from django.contrib import admin
-# from django.urls import path, include
-# from . import views 
-# from django.contrib.auth.views import LoginView,LogoutView
-# from .views import payment,terms,submit,home,aboutus,contact,ProfileView
-
-
-# urlpatterns = [
-    
-#     path("customer-register/",views.CustomerRegisterView.as_view(),name="customer-register"),
-#     path("login/",views.UserLoginView.as_view(),name="login"),
-#     path("customer-dashboard/",views.CustomerDashboardView.as_view(),name="customer-dashboard"),
-#     #path("serviceprovider-register/",views.ServiceproviderRegisterView.as_view(),name="serviceprovider-register"),
-#     path("serviceprovider-dashboard/",views.ServiceproviderDashboardView.as_view(),name="serviceprovider-dashboard"),
-#     path("logout/",LogoutView.as_view(next_page = "/user/login"),name="logout"),
-#     path('home/',home, name='home'),
-#     path('aboutus/',aboutus, name='aboutus'),
-#     path('contact/',contact, name='contact'),
-        # path('submit/',submit, name='submit'),
-        # path('terms/',terms, name='terms'),
-        # path('payment/',payment, name='payment'),
-#     path('invoice/<int:invoice_id>/', views.show_invoice, name='show_invoice'),
-#     path('profile/', ProfileView.as_view(), name='profile'),
-#     path('book-now/<int:item_id>/', views.book_now, name='book_now'),
-    
-# ]
--- user/views.py
+++ user/views.py
@@ -10,6 +10,8 @@ from django.utils import timezone
 
 from .models import User, Item, Booking, Invoice
 from .forms import CustomerRegistrationForm, ServiceproviderRegistrationForm, ProfileForm
+
+
 # Note: Ensure to import any other required models or forms.
 
 # User Registration for Customers
@@ -96,117 +98,11 @@ def book_now(request, item_id):
     else:
         return redirect('login')
 
+
 def show_invoice(request, invoice_id):
     invoice = Invoice.objects.get(id=invoice_id)
     item = invoice.booking.item
     return render(request, 'user/invoice.html', {'invoice': invoice,'item': item})
-
-
-
-
-
-
-# from django.shortcuts import render,redirect
-# from django.views.generic.edit import CreateView
-# from .models import User
-# from .forms import userRegistrationForm, ServiceproviderRegistrationForm
-# #import settings.py
-# from django.conf import settings
-# #send_mail is built-in function in django
-# from django.core.mail import send_mail
-# from django.http import HttpRequest, HttpResponse
-# from .models import User, Item, Booking, Invoice
-# from.forms import userRegistrationForm
-# from django.contrib.auth.views import LoginView
-# from django.views.generic import ListView
-# from service.models import Service
-# from django.views.generic import UpdateView
-# from django.urls import reverse_lazy
-# from django.contrib.auth.mixins import LoginRequiredMixin
-# from .forms import ProfileForm
-# from django.utils import timezone
-
-# # from django.contrib.auth.decorators import login_required
-# # from django.utils.decorators import method_decorator
-
-# # Create your views here.
-# class userRegisterView(CreateView):
-#     template_name = 'user/user_register.html'
-#     model = User
-#     form_class = userRegistrationForm
-#     success_url = '/login/'
-    
-    
-#     def form_valid(self, form):
-#         email = form.cleaned_data.get('email')
-#         #print("email....",email)
-#         if sendMail(email):
-#             print("Mail sent successfully")
-#             return super().form_valid(form)
-#         else:
-#             return super().form_valid(form)
-
-
-# class ServiceproviderRegisterView(CreateView):
-#     template_name = 'user/serviceprovider_register.html'
-#     model = User
-#     form_class = ServiceproviderRegistrationForm
-#     success_url = '/login/'    
-    
-
-# def sendMail(to):
-#     subject = 'Welcome to Urban Service'
-#     message = 'Hope you are enjoying your Django Tutorials'
-#     #recepientList = ["samir.vithlani83955@gmail.com"]
-#     recepientList = [to]
-#     EMAIL_FROM = settings.EMAIL_HOST_USER
-#     send_mail(subject,message, EMAIL_FROM, recepientList)
-#     #attach file
-#     #html
-#     return True
-
-
-# class UserLoginView(LoginView): 
-#     template_name = 'user/login.html'
-#     model = User
-    
-    
-#     def get_redirect_url(self):
-#         if self.request.user.is_authenticated:
-#             if self.request.user.is_user:
-#                 return '/user/user-dashboard/'
-#             else:
-#                 return '/user/serviceprovider-dashboard/'
-            
-
-# class userDashboardView(ListView):
-    
-#     def get(self, request, *args, **kwargs):
-#         #logic to get all the projects
-#         print("userDashboardView")
-#         services = Service.objects.all() #select * from project
-#         print(".............................................",services)
-       
-#         return render(request, 'user/user_dashboard.html',{"services": services})
-    
-    
-#     template_name = 'user/user_dashboard.html'
-
-# class ServiceproviderDashboardView(ListView):
-#     def get(self, request, *args, **kwargs):
-#         #logic to get all the projects
-#         return render(request, 'user/serviceprovider_dashboard.html')
-    
-#     template_name = 'user/serviceprovider_dashboard.html'    
-    
-# def home(request):
-#     return render(request, 'user/home.html')
-
-# def aboutus(request):
-#     return render(request, 'user/aboutus.html')
-
-# def contact(request):
-#     return render(request, 'user/contact.html')
 
 def submit(request):
     return render(request, 'user/submit.html')
@@ -219,41 +115,3 @@ def payment(request):
     return render(request, 'user/payment.html')
 
 
-# def show_invoice(request, invoice_id):
-#     invoice = Invoice.objects.get(id=invoice_id)
-#     return render(request, 'invoice.html', {'invoice': invoice})
-# # def invoice(request):
-# #     return render(request, 'user/invoice.html')
-# # class InvoiceDetailView(DetailView):
-# #     model = Item
-# #     context_object_name = "item"
-# #     template_name = "user/invoice.html"
-
-# def home(request):
-#     items = Item.objects.all()  # Querying all items
-#     return render(request, 'user/home.html', {'items': items})
-
-
-# class ProfileView(LoginRequiredMixin, UpdateView):
-#     model = User
-#     form_class = ProfileForm
-#     template_name = 'user/profile.html'
-#     success_url = reverse_lazy('profile')
-
-#     def get_object(self):
-#         return self.request.user  # Assuming User model is used for profile
-
-
-# def book_now(request, item_id):
-#     if request.method == "POST":
-#         # Assuming the user is authenticated and user details are accessible
-#         user = request.user.user
-#         item = Item.objects.get(id=item_id)
-        
-#         booking = Booking.objects.create(user=user, item=item, booking_date=timezone.now())
-        
-#         # Create an invoice for the booking with the item's price
-#         invoice = Invoice.objects.create(booking=booking, amount=item.price, status="Pending")
-        
-#         # Redirect to an invoice page
-#         return redirect('show_invoice', invoice_id=invoice.id)
